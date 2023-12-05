afebfbc63846edd28938f7188fff434a80140048
Aizu4 <234679@edu.p.lodz.pl>
2023-12-05 16:29:28 +0100

    make the access_check_mixin.py naming consistent with other access methods

--- v1/models/mixins/access_check_mixin.py
+++ v1/models/mixins/access_check_mixin.py
@@ -5,8 +5,8 @@ class AccessCheckMixin:
     user: User
     public: bool
 
-    def is_visible_for(self, user: User) -> bool:
+    def is_visible_by(self, user: User) -> bool:
         return user.is_superuser or self.user == user or self.public
 
-    def is_editable_for(self, user: User) -> bool:
+    def is_editable_by(self, user: User) -> bool:
         return user.is_superuser or self.user == user
