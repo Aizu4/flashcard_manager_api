9920bf5ecd232685897451cf10b27d7e3e50952b
Aizu4 <234679@edu.p.lodz.pl>
2023-12-16 19:26:57 +0100

    add support for generating the example sentences

--- v1/services/__init__.py
+++ v1/services/__init__.py
@@ -1,2 +1,3 @@
 from .csv_service import CSVService
 from .image_service import ImageService
+from .openai import SentenceService
---
+++ v1/services/openai/__init__.py
@@ -0,0 +1,5 @@
+from openai import AsyncOpenAI, OpenAI
+
+openAIClient = OpenAI()
+
+from .sentence_service import SentenceService
